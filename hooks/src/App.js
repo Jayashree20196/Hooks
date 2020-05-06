9d4bbadcb9b729f747a0e7ce2a99f99e7cee97e7
Jayashree Nagarajan <njayashree20196@gmail.com>
2020-05-06 18:23:44 +0530

    used useState and updated useState

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,30 +1,29 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
-/*functional parent component */
-function App() {
-  
-  //calling the ChildComponent
-  return (
-    <ChildComponent/>
-  )
-}
-
-
-/* child functional component called from its parent along with props */
+/* funtional parent component */
+function App (){
+  return <ChildComponent />;
+};
+ 
+/* funtional child component */
 function ChildComponent() {
 
-  //using useState storing the value in const
-  const [useStateText, useStateSetting] = useState(
-    "This text is displayed using useState"
-  )
-
-  //text is displayed from useState
+  //setting const with default text
+  //childText is used to set the input value
+  const [childText, setchildText] = useState(
+    'This text is updated from input!'
+  );
+ 
   return (
-    <div className="App">
-      <h1>{useStateText}</h1>
+    <div>
+      <h1>{childText}</h1>
+      <input
+        type="text"
+        value={childText}
+        onChange={event => setchildText(event.target.value)}
+      />
     </div>
   );
-}
-
+};
+ 
 export default App;
