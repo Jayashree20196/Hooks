513281584378cfc58f9907d2fa821061beaba2eb
Jayashree Nagarajan <njayashree20196@gmail.com>
2020-05-06 16:42:11 +0530

    added simple functional component with a text

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+/*functional component */
 function App() {
+  //declare a const with a string
+  const sampleText = "This is the begining of react hooks"
+
+  //const is directly returned
   return (
     <div className="App">
-     Test
+      <h1>{sampleText}</h1>
     </div>
   );
 }
