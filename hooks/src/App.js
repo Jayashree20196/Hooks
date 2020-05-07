bf1e0552e0c62fa3e12ff09e4f76fe47ad680e0e
Jayashree Nagarajan <njayashree20196@gmail.com>
2020-05-07 20:44:45 +0530

    call back method to pass as param

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,25 +2,28 @@ import React, { useState } from 'react';
 
 /* funtional parent component */
 function App (){
-  return <ChildComponent />;
+  //setting const with default text
+  //parentText is used to set the input value
+  const [parentText, setparentText] = useState(
+    'This text is passed through callback method from parent!'
+  );
+
+  //declaring seperate onchange method
+  const handleparentText = (event) => setparentText(event.target.value)
+
+  return <ChildComponent parentText={parentText} onChangeParent={handleparentText}/>;
 };
  
 /* funtional child component */
-function ChildComponent() {
-
-  //setting const with default text
-  //childText is used to set the input value
-  const [childText, setchildText] = useState(
-    'This text is updated from input!'
-  );
- 
+//the child functional component receives a call back method as a param from parent
+function ChildComponent({parentText, onChangeParent}) {
   return (
     <div>
-      <h1>{childText}</h1>
+      <h1>{parentText}</h1>
       <input
         type="text"
-        value={childText}
-        onChange={event => setchildText(event.target.value)}
+        value={parentText}
+        onChange={onChangeParent}
       />
     </div>
   );
